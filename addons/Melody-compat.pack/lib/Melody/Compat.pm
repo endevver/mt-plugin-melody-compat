64aae68cbdaae7cdafcef80c43cca0668d451e2a
Jay Allen <jay@endevver.com>
2013-08-14 15:43:25 -0700

    Version 1.0.2; Slight performance tweak, eliminating reprocessing of plugins

--- addons/Melody-compat.pack/lib/Melody/Compat.pm
+++ addons/Melody-compat.pack/lib/Melody/Compat.pm
@@ -1,14 +1,16 @@
 package Melody::Compat;
 
-use base qw(Class::Data::Inheritable);
+use base qw( Class::Data::Inheritable );
+
 Melody::Compat->mk_classdata( is_disabled => 0 );
 
 sub init_app {
     my ( $cb, $app ) = @_;
     return if __PACKAGE__->is_disabled();
-    # Operate only on MT::App subclassed apps that can't query()
-    return unless $app and $app->isa('MT::App');
-    return if $app->can('query');
+
+    # Operate only on MT::App subclasses that can't query()
+    return unless eval { $app->isa('MT::App') }
+               && ! defined($app->can('query'));
 
     # Install definition for MT::App::query() as it is in Melody
     *{ref($app)."::query"} = sub {
@@ -22,9 +24,9 @@ sub init_app {
 }
 
 # This callback is invoked when MT is initialized and ready to run.
-# This callback is invoked after MT initialized addons, plugins, schema
-# and permissions.  The arguments passed to initialize MT is passed
-# through to the callback.
+# More specifically, after MT has initialized its addons, plugins,
+# schema and permissions.  The arguments used to initialize MT are
+# passed through to the callback.
 sub post_init {
     my ($cb, $param) = @_;
 
@@ -32,10 +34,14 @@ sub post_init {
     __PACKAGE__->is_disabled( MT->product_code() eq 'OM' );
     return if __PACKAGE__->is_disabled();
 
+    require Scalar::Util;
+
+    my %seen;
     foreach my $c ( MT::Component->select(), MT::Plugin->select() ) {
+        next if $c->id eq 'core' || $seen{Scalar::Util::refaddr($c)}++;
         my $plugin_class = eval { $c->registry('plugin_class') }
                         || eval { $c->{registry}{plugin_class} };
-        if ($plugin_class) {
+        if ( $plugin_class ) {
             eval "require $plugin_class;";
             die $@ if $@;
             bless $c, $plugin_class;
